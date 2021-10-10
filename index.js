05e9c107b65bd50d86ee3bdfd56e4a97c265c8ac
Showgam <84779605+Showgam@users.noreply.github.com>
2021-10-10 18:39:55 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const { Client, Collection, Intents } = require('discord.js');
+const token;
 
 const handleCommand = require('./helpers/command');
 const handleSelectMenu = require('./helpers/select-menu');
